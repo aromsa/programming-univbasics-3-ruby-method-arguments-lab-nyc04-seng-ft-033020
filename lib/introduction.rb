2ae03cb87ee688c1e78ec2245bf75b8f24d37dfb
anna bateman <annacbateman@gmail.com>
2020-03-22 21:41:54 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -7,8 +7,6 @@ def introduction (name)
   puts "Hi, my name is #{name}."
 end
 
-introduction ("John")
-
 def introduction_with_language (name, language)
   puts "Hi, my name is #{name} and I am leaning to program in #{language}."
 end
